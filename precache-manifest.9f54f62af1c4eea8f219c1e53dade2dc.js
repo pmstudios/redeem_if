21823a89bc1341bea752d305b0250e68aeb50ba8
limdp99 <47866457+limdp99@users.noreply.github.com>
2020-09-01 16:15:03 +0900

    Updates

---
+++ precache-manifest.9f54f62af1c4eea8f219c1e53dade2dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba442aba8f8d7ab062776fa7bcf64b60",
+    "url": "/redeem_if/index.html"
+  },
+  {
+    "revision": "79abe4c0cf574f438854",
+    "url": "/redeem_if/static/css/main.c7996994.chunk.css"
+  },
+  {
+    "revision": "41a41b3ad6447ec705cd",
+    "url": "/redeem_if/static/js/2.60315b07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/redeem_if/static/js/2.60315b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79abe4c0cf574f438854",
+    "url": "/redeem_if/static/js/main.b28d500b.chunk.js"
+  },
+  {
+    "revision": "df5d1cfab0878fb802fc",
+    "url": "/redeem_if/static/js/runtime-main.3cf0f778.js"
+  }
+]);
